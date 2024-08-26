5cc2ee24aafb926287965aa6ba63b89dfc49b322
sainane <rachel.tranchida@gmail.com>
2024-08-26 14:04:51 +0200

    add pages routes

---
+++ src/pages/AddPlant.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function AddPlant() {
+    return (
+        <div>
+            <h1>
+                This is plant page
+            </h1>
+        </div>
+    )
+}
+
+export default AddPlant
