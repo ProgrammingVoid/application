acaf4348f337f2325a0fc18f8997530478154cd3
evarayHEIG <eva.ray@heig-vd.ch>
2024-08-27 09:26:26 +0200

    Add sign up button component and rename signin -> signup

---
+++ src/components/SignUpButton.tsx
@@ -0,0 +1,37 @@
+/**
+ * Project Name: PlantKeeper
+ *
+ * @created 26-08-2024
+ * @file SignUpButton.tsx
+ * @version 1.0.0
+ * @see https://github.com/Plant-keeper
+ *
+ * @authors
+ *   - Rafael Dousse
+ *   - Eva Ray
+ *   - Quentin Surdez
+ *   - Rachel Tranchida
+ */
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import RoundedButton from "./RoundedButton";
+
+const SignInButton = () => {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate('/signup');
+    };
+
+    return (
+        <RoundedButton
+            text="Sign Up"
+            textColor="text-green-900"
+            bgColor="bg-gray-50"
+            onClick={handleSignInClick}
+        />
+    );
+}
+
+export default SignInButton;
