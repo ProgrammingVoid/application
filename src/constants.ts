460e97b00f615c47fd82a2a7f51048e644465d14
sainane <rachel.tranchida@gmail.com>
2024-08-28 11:02:40 +0200

    add constants file

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * Project Name: PlantKeeper
+ *
+ * @created 28/08/2024
+ * @file constants.ts
+ * @version 1.0.0
+ * @see https://github.com/Plant-keeper
+ *
+ * @authors
+ *   - Rafael Dousse
+ *   - Eva Ray
+ *   - Quentin Surdez
+ *   - Rachel Tranchida
+ */
+export const API_URL = "http://localhost:8080";
+export const SENSOR_URL = "/sensor";
+export const PLANT_URL = "/plant";
+export const USER_URL = "/user";
+export const LOGIN_URL = "/login";
+export const REGISTER_URL = "/register";
