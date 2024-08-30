eb986dc1f66342fd16dcc1d2e95d35ec967cf954
sainane <rachel.tranchida@gmail.com>
2024-08-30 16:09:00 +0200

    fix token name on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import LoginForm from "../components/LoginForm";
 function Login() {
     return (
         <div>
             <h1>
                 This is Login page
             </h1>
+            <LoginForm/>
         </div>
     )
 }
