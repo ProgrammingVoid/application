b99eb95b7b9e46238e5a751c099f87d0392733d6
sainane <rachel.tranchida@gmail.com>
2024-08-31 16:05:37 +0200

    more info button pass plant id

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {useLocation} from "react-router-dom";
+
 function Info() {
+    const location = useLocation();
+
+    const id = location.state.plantId;
     return (
         <div>
             <h1>
                 This is Info page
+                {id}
             </h1>
         </div>
     )
