56ddb9108b7ae8d8e4d8c58b40eabc3f4d0a934a
sainane <rachel.tranchida@gmail.com>
2024-09-01 10:22:55 +0200

    login page fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import LoginForm from "../components/LoginForm";
-import AuthNavbar from "../components/AuthNavbar";
+import NotAuthNavbar from "../components/NotAuthNavbar";
 function Login() {
     return (
         <div>
-            <AuthNavbar/>
+            <NotAuthNavbar/>
             <div className="flex justify-center items-center mt-32">
                 <LoginForm/>
             </div>
