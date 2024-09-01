af9aef2e60beedb5431822f64513ddb6d4226fa6
sainane <rachel.tranchida@gmail.com>
2024-09-01 12:40:01 +0200

    sensor page with new route added link to assigned plant

--- src/pages/Sensors.tsx
+++ src/pages/Sensors.tsx
@@ -1,62 +1,31 @@
 import SensorDescription from "../components/SensorDescription";
 import AuthNavbar from "../components/AuthNavbar";
 import RoundedButton from "../components/RoundedButton";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {API_URL, USER_PLANTS_URL, USER_SENSORS_URL} from "../constants";
+import {API_URL, USER_SENSOR_LINKED_PLANTS} from "../constants";
 import {useNavigate} from "react-router-dom";
 import Cookies from "js-cookie";
+import {SensorLinkedToPlant} from "../types";
 
-interface Sensor {
-    id: number;
-    name: string;
-    remark: string;
-    plantName: string | null;
-    plantId: number;
-
-
-}
-
-interface Plant {
-    id: number,
-    name: string,
-    sensor: {
-        id: number,
-    }
-}
 
 function Sensors() {
-    const [sensors, setSensors] = useState<Sensor[]>([]);
+    const [sensors, setSensors] = useState<SensorLinkedToPlant[]>([]);
     const navigate = useNavigate();
-    const plantsRef = useRef<Plant[]>([]);
     const handleAddSensorClick = () => {
         navigate('/addsensor');
     }
 
-    const mapSensorsToPlants = (sensors: Sensor[], plants: Plant[]) => {
-        return sensors.map(sensor => {
-            const plant = plants.find(plant => plant.sensor.id === sensor.id);
-
-            return {
-                ...sensor,
-                plantName: plant ? plant.name : null,
-            };
-        });
-    };
-
     useEffect(() => {
-        axios.get(API_URL + USER_PLANTS_URL, { headers: { Authorization: `Bearer ${Cookies.get('token')}` } })
-            .then((response) => {
-                plantsRef.current = response.data;
-                return axios.get(API_URL + USER_SENSORS_URL, { headers: { Authorization: `Bearer ${Cookies.get('token')}` } });
-            })
-            .then((response) => {
-                const sensorsData = response.data;
-                const mappedSensors = mapSensorsToPlants(sensorsData, plantsRef.current);
-                setSensors(mappedSensors);
-            })
-            .catch(error => console.error(error));
-    }, []);
+            axios.get(API_URL + USER_SENSOR_LINKED_PLANTS, {headers: {Authorization: `Bearer ${Cookies.get('token')}`}})
+                .then((response) => {
+                   const sensorsData = response.data;
+                   console.log(sensorsData);
+                    setSensors(sensorsData);
+                })
+                .catch(error => console.error(error));
+        }
+        , []);
     return (
         <div>
             <AuthNavbar></AuthNavbar>
@@ -69,8 +38,8 @@ function Sensors() {
             </div>
             <div className="mx-12">
                 {sensors.map(sensor => (
-                    <div key={sensor.id} className="mb-8">
-                        <SensorDescription plantName={sensor.plantName} id={sensor.id} description={sensor.remark} name={sensor.name}/>
+                    <div key={sensor.sensorId} className="mb-8">
+                        <SensorDescription  info={sensor}/>
                     </div>
                 ))}
             </div>
