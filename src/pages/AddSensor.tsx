eb880353bcf63e1a7d20ba05fd83b239ea211652
sainane <rachel.tranchida@gmail.com>
2024-09-01 10:35:53 +0200

    sensor page fix center position

--- src/pages/AddSensor.tsx
+++ src/pages/AddSensor.tsx
@@ -3,11 +3,14 @@ import SensorForm from "../components/SensorForm";
 import AuthNavbar from "../components/AuthNavbar";
 function AddSensor() {
     return (
-        <div>
-            <AuthNavbar/>
-
-            <div className={"flex flex-row justify-center mt-36"}>
-                <SensorForm/>
+        <div className="min-h-screen w-full flex flex-col items-center">
+            <div className="w-full">
+                <AuthNavbar/>
+            </div>
+            <div className="flex flex-col items-center justify-center flex-grow min-h-0">
+                <div className="m-14 flex flex-col justify-center items-center w-full h-full">
+                    <SensorForm></SensorForm>
+                </div>
             </div>
         </div>
     )
