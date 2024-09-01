b129dfa58ecac2c4901968be754c90cd4802b951
sainane <rachel.tranchida@gmail.com>
2024-09-01 10:37:17 +0200

    login page position of form centered

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,14 @@ import LoginForm from "../components/LoginForm";
 import NotAuthNavbar from "../components/NotAuthNavbar";
 function Login() {
     return (
-        <div>
-            <NotAuthNavbar/>
-            <div className="flex justify-center items-center mt-32">
-                <LoginForm/>
+        <div className="min-h-screen w-full flex flex-col items-center">
+            <div className="w-full">
+                <NotAuthNavbar/>
+            </div>
+            <div className="flex flex-col items-center justify-center flex-grow min-h-0">
+                <div className="m-14 flex flex-col justify-center items-center w-full h-full">
+                    <LoginForm></LoginForm>
+                </div>
             </div>
         </div>
     )
