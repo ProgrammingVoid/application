b450a1a6474201d4600ac925e5da0ea06a22608a
sainane <rachel.tranchida@gmail.com>
2024-09-01 14:19:32 +0200

    add plant with generalPlantId

--- src/constants.ts
+++ src/constants.ts
@@ -21,3 +21,4 @@ export const USER_SENSORS_URL = USER_URL + "/sensors";
 export const USER_PLANTS_URL = USER_URL + PLANT_URL;
 export const LOGIN_URL = "/auth/login";
 export const REGISTER_URL = "/register";
+export const GENERAL_PLANTS_URL = "/general-plants";
--- src/pages/AddPlant.tsx
+++ src/pages/AddPlant.tsx
@@ -1,27 +1,29 @@
 import React, {useEffect, useState} from 'react'
 import PlantForm from "../components/PlantForm";
 import axios from "axios";
-import {API_URL, USER_SENSORS_URL} from "../constants";
+import {API_URL, GENERAL_PLANTS_URL, USER_SENSORS_URL} from "../constants";
 import AuthNavbar from "../components/AuthNavbar";
-import {SensorInfo} from "../types";
+import {GeneralPlant, SensorInfo} from "../types";
 import Cookies from "js-cookie";
 
 function AddPlant() {
     const [sensorInfos, setSensorInfos] = useState<SensorInfo[]>([]);
+    const [plantTypes, setPlantTypes] = useState<GeneralPlant[]>([]);
+
     useEffect(() => {
 
         axios.get(API_URL + USER_SENSORS_URL, {headers: {Authorization: `Bearer ${Cookies.get('token')}`}})
-            .then((response) => { const sensors = response.data;
-                const sensorInfo = sensors.map((sensor: { name: string, id: number}) => ({name: sensor.name, id: sensor.id}));
-                setSensorInfos(sensorInfo);});
-
+            .then((response) => setSensorInfos(response.data));
+        axios.get(API_URL + GENERAL_PLANTS_URL)
+            .then((response) => setPlantTypes(response.data))
+            .catch(error => console.error(error));
     }, []);
     return (
         <div>
             <AuthNavbar/>
 
             <div className={"flex flex-row justify-center mt-24"}>
-                <PlantForm plantTypeOptions={['Monstera']} sensorOptions={sensorInfos}/>
+                <PlantForm plantTypeOptions={plantTypes} sensorOptions={sensorInfos}/>
             </div>
         </div>
     )
