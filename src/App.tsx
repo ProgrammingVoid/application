c4c9ead620b629bb7692a2280838321ed4c0379b
ProgrammingVoid <rachel@tranchida.ch>
2024-09-05 19:33:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,14 +27,6 @@ function App() {
     document.head.appendChild(link);
   }, []);
 
-  // Set the favicon of the page
-  useEffect(() => {
-    const link = document.createElement('link');
-    link.rel = 'icon';
-    link.href = require("./figures/icon.png");
-    document.head.appendChild(link);
-  }, []);
-
   // Set the title of the page
   useEffect(() => {
     document.title = "PlantKeeper";
